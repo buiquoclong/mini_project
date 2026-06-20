c8394b37f6a8efce7c921f09aa57fa44ba2cc21f
buiquoclong <buiquoclongkt123@gmail.com>
2026-06-20 23:14:46 +0700

    Update logic and comment in useLocalStorage

--- mini_project/src/hooks/useLocalStorage.ts
+++ mini_project/src/hooks/useLocalStorage.ts
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 
+// Luu tru gia tri vao localStorage va lay gia tri tu localStorage
 export function useLocalStorage<T>(key: string, initialValue: T) {
   const [value, setValue] = useState<T>(() => {
     const stored = localStorage.getItem(key);
