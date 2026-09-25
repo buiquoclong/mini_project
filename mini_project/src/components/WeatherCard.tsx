6e43eaf59984e227ae705fd9fb95d8b9d895d0cd
buiquoclong <buiquoclongkt123@gmail.com>
2026-09-25 23:54:28 +0700

    Update logic and comment in WeatherCard of mini_project

--- mini_project/src/components/WeatherCard.tsx
+++ mini_project/src/components/WeatherCard.tsx
@@ -78,7 +78,7 @@ export default function WeatherCard({ data }: Props) {
               </p>
             </div>
 
-            <div className="rounded-2xl border border-white/10 bg-black/10 p-4 transition-colors hover:bg-white/[0.06]">
+            <div className="rounded-2xl border border-white/10 bg-black/10 p-4 transition-colors hover:bg-white/6">
               <div className="flex items-center gap-2 text-zinc-500">
                 <Compass size={15} />
                 <span className="text-xs">Direction</span>
